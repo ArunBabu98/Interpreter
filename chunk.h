3f8b544fe3fae0a6d54c270c3159be303aace06c
ArunBabu98 <arun.b@changeangel.io>
2024-03-25 23:51:55 +0530

    Adding constant value array to the chunk(bytecode)

--- chunk.h
+++ chunk.h
@@ -2,10 +2,12 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 // bytecode farmat, each instruction has a one-byte operation code, a.k.a opcode
 
 typedef enum
 {
+    OP_CONSTANT,
     OP_RETURN, // return from the current function
 } OpCode;
 
@@ -14,10 +16,12 @@ typedef struct
     int count;
     int capacity;
     uint8_t *code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
-void freeChunk(Chunk* chunk);
+void freeChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte);
+int addConstant(Chunk *chunk, Value value);
 
 #endif
